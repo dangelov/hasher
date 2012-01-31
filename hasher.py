97ebe628bbb271267a491dbd3178beffd4fb4136
Dino Angelov <dino@dangelov.com>
2012-01-30 21:11:13 -0600

    added SHA1 option

--- hasher.py
+++ hasher.py
@@ -16,3 +16,15 @@ class Md5Command(sublime_plugin.TextCommand):
             m.update(selected)
             txt = m.hexdigest()
             self.view.replace(edit, s, txt)
+
+class Sha1Command(sublime_plugin.TextCommand):
+    def run(self, edit):
+        for s in self.view.sel():
+            if s.empty():
+                s = self.view.word(s)
+
+            selected = self.view.substr(s)
+            m = hashlib.sha1()
+            m.update(selected)
+            txt = m.hexdigest()
+            self.view.replace(edit, s, txt)
