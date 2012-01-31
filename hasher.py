bbee6a2f51b210d266cdc66c9b2a0f0e7db3a9c5
Dino Angelov <dino@dangelov.com>
2012-01-30 20:50:58 -0600

    finally

---
+++ hasher.py
@@ -0,0 +1,18 @@
+import string
+import sublime
+import sublime_plugin
+import hashlib
+
+# test
+
+class Md5Command(sublime_plugin.TextCommand):
+    def run(self, edit):
+        for s in self.view.sel():
+            if s.empty():
+                s = self.view.word(s)
+
+            selected = self.view.substr(s)
+            m = hashlib.md5()
+            m.update(selected)
+            txt = m.hexdigest()
+            self.view.replace(edit, s, txt)
