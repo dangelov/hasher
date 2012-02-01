f9589d4e6ce338828ef5bbf5dea5cd554d591131
Dino Angelov <dino@dangelov.com>
2012-02-01 11:08:22 -0600

    Added URL Encode and Decode functions

--- hasher.py
+++ hasher.py
@@ -2,8 +2,9 @@ import string
 import sublime
 import sublime_plugin
 import hashlib
+import urllib
 
-# test
+# test%20me%3B%28%29
 
 class Md5Command(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -27,4 +28,24 @@ class Sha1Command(sublime_plugin.TextCommand):
             m = hashlib.sha1()
             m.update(selected)
             txt = m.hexdigest()
+            self.view.replace(edit, s, txt)
+
+class UrlEncodeCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        for s in self.view.sel():
+            if s.empty():
+                s = self.view.word(s)
+
+            selected = self.view.substr(s)
+            txt = urllib.quote(selected)
+            self.view.replace(edit, s, txt)
+
+class UrlDecodeCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        for s in self.view.sel():
+            if s.empty():
+                s = self.view.word(s)
+
+            selected = self.view.substr(s)
+            txt = urllib.unquote(selected)
             self.view.replace(edit, s, txt)
